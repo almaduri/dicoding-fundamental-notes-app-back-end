27f7fb04e49c9ab4907c7e993fe03ba13e54e41c
almaduri <ali.mdrw@gmail.com>
2022-03-05 21:25:41 +0800

    add try catch in getNoteByIdHandler

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -38,19 +38,28 @@ class NoteHandler {
     };
   }
 
-  getNoteByIdHandler(request) {
-    const { id } = request.params;
-    const note = this._service.getNoteById(id);
-    return{
-      status: 'success',
-      data: {
-        note,
-      },
-    };
+  getNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const note = this._service.getNoteById(id);
+      return {
+        status: 'success',
+        data: {
+          note,
+        },
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(404);
+      return response;
+    }
   }
 
   putNoteByIdHandler() {
-    
+
   }
 
   deleteNoteByIdHandler() {
